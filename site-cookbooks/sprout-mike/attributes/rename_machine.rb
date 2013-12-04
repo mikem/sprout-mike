47e0f351eb62fe9f47908d77ff86aaab8f1f9b74
Mike Mazur <mmazur@gmail.com>
2013-12-03 21:21:00 -0800

    Only rename machine if new hostname set

--- site-cookbooks/sprout-mike/attributes/rename_machine.rb
+++
@@ -1,2 +0,0 @@
-raise "Set the MACHINE_HOSTNAME environment variable first!" unless ENV.has_key? 'MACHINE_HOSTNAME'
-node.default['desired_hostname'] = ENV['MACHINE_HOSTNAME']
--- site-cookbooks/sprout-mike/recipes/rename_machine.rb
+++ site-cookbooks/sprout-mike/recipes/rename_machine.rb
@@ -1,19 +1,19 @@
-hostname = node['desired_hostname']
-raise "The hostname '#{hostname}' is not valid" if hostname.empty?
+if ENV.has_key?('MACHINE_HOSTNAME') && !ENV['MACHINE_HOSTNAME'].empty?
+  # The scutil commands need to run as root, unless
+  # you're logged into the console, but we can't be sure of that.
 
-# The scutil commands need to run as root, unless
-# you're logged into the console, but we can't be sure of that.
+  hostname = ENV['MACHINE_HOSTNAME']
+  ["scutil --set ComputerName #{hostname}",
+   "scutil --set LocalHostName #{hostname}",
+   "scutil --set HostName #{hostname}",
+   "hostname #{hostname}",
+   "diskutil rename / #{hostname}" ].each do |host_cmd|
+    execute host_cmd
+  end
 
-["scutil --set ComputerName #{hostname}",
- "scutil --set LocalHostName #{hostname}",
- "scutil --set HostName #{hostname}",
- "hostname #{hostname}",
- "diskutil rename / #{hostname}" ].each do |host_cmd|
-  execute host_cmd
-end
-
-ruby_block "test to see if hostname was set" do
-  block do
-    raise "Setting of hostname failed" unless hostname == `scutil --get ComputerName`.chop
+  ruby_block "test to see if hostname was set" do
+    block do
+      raise "Setting of hostname failed" unless hostname == `scutil --get ComputerName`.chop
+    end
   end
 end
