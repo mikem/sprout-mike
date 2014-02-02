853885b83460755609b83f5a7573ac01b853b73e
Mike Mazur <mmazur@gmail.com>
2014-02-01 18:35:44 -0800

    Only run NeoMail scripts if app doesn't already exist

--- site-cookbooks/sprout-mike/recipes/neo-mail.rb
+++ site-cookbooks/sprout-mike/recipes/neo-mail.rb
@@ -13,52 +13,53 @@ exec_path="#{app_root}/#{name}.app/Contents/MacOS"
 profile_path="#{app_root}/#{name}.app/Contents/Profile"
 plist_path="#{app_root}/#{name}.app/Contents/Info.plist"
 
-["#{app_root}/#{name}.app",
- "#{app_root}/#{name}.app/Contents",
- resource_path,
- exec_path,
- profile_path].each do |dir|
-  directory dir do
-    action :create
-    owner node['current_user']
-    group 'staff'
+unless File.exists? "#{app_root}/#{name}.app"
+  ["#{app_root}/#{name}.app",
+   "#{app_root}/#{name}.app/Contents",
+   resource_path,
+   exec_path,
+   profile_path].each do |dir|
+    directory dir do
+      action :create
+      owner node['current_user']
+      group 'staff'
+    end
   end
-end
 
-directory node['neo-mail']['icon-dir'] do
-  owner node['current_user']
-end
+  directory node['neo-mail']['icon-dir'] do
+    owner node['current_user']
+  end
 
-cookbook_file node['neo-mail']['icon-filename'] do
-  path icon
-  owner node['current_user']
-  mode '0644'
-end
+  cookbook_file node['neo-mail']['icon-filename'] do
+    path icon
+    owner node['current_user']
+    mode '0644'
+  end
 
-bash 'convert icon' do
-  user node['current_user']
-  code <<-BASH
-    if [ -f #{icon} ] ; then
-      sips -s format tiff #{icon} --out #{resource_path}/icon.tiff --resampleWidth 128 >& /dev/null
-      tiff2icns -noLarge #{resource_path}/icon.tiff >& /dev/null
-    fi
-  BASH
-end
+  bash 'convert icon' do
+    user node['current_user']
+    code <<-BASH
+      if [ -f #{icon} ] ; then
+        sips -s format tiff #{icon} --out #{resource_path}/icon.tiff --resampleWidth 128 >& /dev/null
+        tiff2icns -noLarge #{resource_path}/icon.tiff >& /dev/null
+      fi
+    BASH
+  end
 
-bash 'create executable' do
-  user node['current_user']
-  code <<-BASH
+  bash 'create executable' do
+    user node['current_user']
+    code <<-BASH
 cat >#{exec_path}/#{name} <<EOF
 #!/bin/sh
 exec "#{chrome_path}" --app="#{url}" --user-data-dir="#{profile_path}" "\\$@"
 EOF
 chmod +x #{exec_path}/#{name}
-  BASH
-end
+    BASH
+  end
 
-bash 'create Info.plist' do
-  user node['current_user']
-  code <<-BASH
+  bash 'create Info.plist' do
+    user node['current_user']
+    code <<-BASH
 cat > #{plist_path} <<EOF
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" “http://www.apple.com/DTDs/PropertyList-1.0.dtd”>
@@ -71,5 +72,6 @@ cat > #{plist_path} <<EOF
 </dict>
 </plist>
 EOF
-  BASH
+    BASH
+  end
 end
