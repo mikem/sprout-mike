ad8b3dd3ce8c244811972084d257f1caeebbec21
Mike Mazur <mmazur@gmail.com>
2014-03-27 15:30:54 -0700

    Install RVM

---
+++ site-cookbooks/sprout-mike/attributes/rvm.rb
@@ -0,0 +1,12 @@
+node.normal['versions']['rvm'] = '8d9d3c34813a91b42e2bbdb07744133ceb027508'
+node.normal["rvm"] ||= {}
+node.normal["rvm"]["rubies"] = {
+  # "ree-1.8.7-2012.02" => { :env => "CC=gcc-4.2" },
+  # "ruby-1.8.7-p358" => { :env => "CC=gcc-4.2" },
+  # "ruby-1.9.2-p290" => { :env => "CC=gcc-4.2" },
+  # "ruby-1.9.3-p194" => { :command_line_options => "--with-gcc=clang" }
+  "ruby-2.1.1" => { :command_line_options => "--verify-downloads 1" }
+}
+
+node.normal["rvm"]["default_ruby"] = "ruby-2.1.1"
+
